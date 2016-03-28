9369aa95a1c531cb4dbc7b0a1b14fc959d6ef9e5
Wayne Witzel III <wayne@riotousliving.com>
2016-03-28 10:42:17 -0400

    added certificate installation and moved to mako

--- rlfarm/views.py
+++ rlfarm/views.py
@@ -9,7 +9,7 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/mytemplate.mako')
 def my_view(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
