714c1fbd1c8ae11c26e9baf012c01a7075ad852f
Wayne Witzel III <wayne@riotousliving.com>
2016-03-30 10:56:31 -0400

    fix pyramid_stripe typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = [
     'pyramid',
     'pyramid_mako',
-    'pyramid_stipe',
+    'pyramid_stripe',
     'pyramid_debugtoolbar',
     'pyramid_tm',
     'SQLAlchemy',
